33bd35b37c3d99f8742482a0b67783eb0c5a4716
JasonJCpeng <jasonjcpeng@hotmail.com>
2017-05-24 10:51:08 +0800

    Rechart

--- dev/js/components/content/custom-table/custom-table-demo.js
+++ dev/js/components/content/custom-table/custom-table-demo.js
@@ -1,9 +1,64 @@
 import React from 'react';
+import {LineChart,Line,YAxis,XAxis,Tooltip,CartesianGrid,Legend,PieChart, Pie, Sector, Cell} from 'recharts';
+
 
 class CustomTableDemo extends React.Component{
 
     render(){
-        return <div>自定义表格</div>;
+        const data1 = [
+            {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+            {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+            {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+            {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+            {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+            {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+            {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+        ];
+        const data = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
+            {name: 'Group C', value: 300}, {name: 'Group D', value: 200}];
+        const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+        const RADIAN = Math.PI / 180;
+        const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+            const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+            const x  = cx + radius * Math.cos(-midAngle * RADIAN);
+            const y = cy  + radius * Math.sin(-midAngle * RADIAN);
+
+            return (
+                <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} 	dominantBaseline="central">
+                    {`${(percent * 100).toFixed(0)}%`}
+                </text>
+            );
+        };
+
+        return <div className="content-container animation-fadeInRight">
+            <div style={{float:'left'}}><LineChart width={500} height={300} data={data1}
+                            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                <XAxis dataKey="name"/>
+                <YAxis/>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <Tooltip/>
+                <Legend />
+                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart></div>
+            <div style={{float:'left'}}> <PieChart width={500} height={400} onMouseEnter={this.onPieEnter}>
+                <Pie
+                    data={data}
+                    cx={300}
+                    cy={200}
+                    labelLine={false}
+                    label={renderCustomizedLabel}
+                    outerRadius={80}
+                    fill="#8884d8"
+                >
+                    {
+                        data.map((entry, index) => <Cell key={index} fill={COLORS[index%COLORS.length]}/>)
+                    }
+                </Pie>
+            </PieChart></div>
+
+        </div>;
     }
 }
 
